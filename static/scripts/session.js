b5be36b9aaaa45f9a2add83f2a14ce06d18fb11a
Alvin Wan <alvinwan1@gmail.com>
2018-10-06 22:05:34 -0700

    finished basic live preview

--- static/scripts/session.js
+++ static/scripts/session.js
@@ -1,18 +1,36 @@
-console.log("Loaded session")
 function Session() {
 
   var client = new Client();
+  var isHost = false;
+  var isViewer = true;
 
   this.start = function() {
     client.register(this);
-    client.newHost();
+    if (mobilecheck()) {
+      client.newHost();
+      isHost = true;
+      isViewer = false;
+    } else {
+      client.newPreview();
+      isHost = false;
+      isViewer = true;
+    }
+  }
+
+  this.registerCamera = function(camera) {
+    this.camera = camera
   }
 
   this.onMove = function(playerId, position, rotation) {
-    client.onMove(playerId, position, rotation);
+    if (isHost) {
+      client.onMove(playerId, position, rotation);
+    }
   }
 
   this.movePlayer = function(playerId, position, rotation) {
-    console.log(playerId, position, rotation)
+    if (isViewer) {
+      this.camera.setAttribute('rotation', rotation);
+      this.camera.setAttribute('position', position);
+    }
   }
 }
